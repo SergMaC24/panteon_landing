da9b377d1e9d8ca243ebf30c8a99a1fab7efb757
SergMac24 <serg808507@gmail.com>
2023-03-17 10:21:02 +0200

    commit4: gallery

--- script.js
+++ script.js
@@ -9,3 +9,54 @@ if (burger && menu) {
 		decor.classList.toggle('_active');
 	});
 }
+
+// gallery 
+var swiper1 = new Swiper(".slider-top", {
+	slidesPerView:1.5,
+	spaceBetween: 20,
+
+	scrollbar: {
+		el: ".swiper-scrollbar",
+		draggable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next-unique-top',
+		prevEl: '.swiper-button-prev-unique-top'
+	},
+	breakpoints: {
+		// when window width is >= 992px
+		992: {
+		  slidesPerView: 4,
+		  spaceBetween: 30
+		},
+		545: {
+			slidesPerView: 2.5,
+			spaceBetween: 30
+		  },
+	  }
+});
+
+var swiper2 = new Swiper(".slider-bottom", {
+	slidesPerView:1.25,
+	spaceBetween: 10,
+
+	scrollbar: {
+		el: ".swiper-scrollbar",
+		draggable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next-unique-bot',
+		prevEl: '.swiper-button-prev-unique-bot'
+	},
+	breakpoints: {
+		// when window width is >= 992px
+		992: {
+		  slidesPerView: 3,
+		  spaceBetween: 10
+		},
+		545: {
+			slidesPerView: 2,
+			spaceBetween: 10
+		  },
+	  }
+});
